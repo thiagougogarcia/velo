003141f2cd60d5279648622ab1cc99816605f4cb
Thiago Garcia <thiagougogarcia@gmail.com>
2026-06-26 14:24:50 -0300

    Padrão Playwright Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,22 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-test('deve consultar um pedido aprovado', async ({ page }) => {
+ //AAA - Arrange, Act, Assert 
+ 
+test('deve consultar um pedido aprovado', async ({ page }) => { 
+  //Arrange - preparar
   await page.goto('http://localhost:5173/');
-
   // Checkpoint 
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
-
-//a[text()='Consultar Pedido'] - XPath
-//a href='/lookup' - CSS Selector
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-
-  // Checkpoint
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
+  //Act - agir
   await page.getByTestId('search-order-id').fill('VLO-X2E61N');
-
   await page.getByTestId('search-order-button').click();
 
+  //Assert - validar
   await expect(page.getByTestId('order-result-id')).toBeVisible();
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-X2E61N');
   
